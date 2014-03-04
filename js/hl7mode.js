4c0168a9fbb9e866c7f3edf296786f7e5c552062
Michael D. Lambert <mlambert125@live.com>
2014-03-03 21:36:47 -0500

    Added syntax highlighting for message type

--- js/hl7mode.js
+++ js/hl7mode.js
@@ -1,22 +1,35 @@
 CodeMirror.defineMode("hl7", function (options) {
     return {
-        startState: function () { return { fieldSeparator: '|', segmentName: null }; },
+        startState: function () { return { fieldSeparator: '|', firstSepEncountered: false, segmentName: '', currentField: 0 }; },
         token: function (stream, state) {
             if(stream.sol()) {
-                stream.eatWhile(function(c) { return c != state.fieldSeparator; });
-                return "string";
-            } else {
-                
-                if(stream.next() == state.fieldSeparator)
-                    return "comment";
-                else {
-                    stream.eatWhile(function(c) { return c != state.fieldSeparator; });
-                    return null;
+                state.currentField = 0;
+                state.firstSepEncountered = false;
+                state.segmentName = '';
+            }
+
+            if (!state.firstSepEncountered) {
+                var curChar = stream.eat(function(c) { return c != state.fieldSeparator; });
+                if (curChar != null) {
+                    state.segmentName += curChar;
+                    return "string";    
                 }
             }
-        },
-    }
 
+            state.firstSepEncountered = true;
+            if (stream.next() == state.fieldSeparator) {
+                state.currentField += 1;
+                return "comment";
+            } else {
+                stream.eatWhile(function(c) { return c != state.fieldSeparator; });
+
+                if (state.segmentName == "MSH" && state.currentField == 8)
+                    return "variable";
+                else
+                    return null;
+            }
+        }
+    }
 });
 
 CodeMirror.defineMIME("text/hl7", "hl7");
