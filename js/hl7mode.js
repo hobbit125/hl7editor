a77cd93bdc4c91965937a03f494699e2ff8e9e73
mlambert <mlambert@dolbey.com>
2014-03-04 14:04:39 -0500

    A lot more work to get a version 1 done

--- js/hl7mode.js
+++ js/hl7mode.js
@@ -1,27 +1,109 @@
 CodeMirror.defineMode("hl7", function (options) {
     return {
-        startState: function () { return { fieldSeparator: '|', firstSepEncountered: false, segmentName: '', currentField: 0 }; },
+        startState: function () { 
+            return { 
+                fieldSeparator: '|', 
+                componentSeparator: '^',
+                fieldRepeater: '~',
+                escapeCharacter: '\\',
+                subComponentSeparator: '&',
+
+                atSep1: false,
+                atSep2: false,
+                atSep3: false,
+                atSep4: false,
+                atSep5: false,
+
+                firstSepEncountered: false, 
+                segmentName: '', 
+                currentField: 0 
+            }; 
+        },
         token: function (stream, state) {
-            if(stream.sol()) {
+            if (state.atSep1) {
+                state.fieldSeparator = stream.next();
+                state.atSep1 = false;
+                state.atSep2 = true;
+                state.firstSepEncountered = true;
+                state.currentField += 1;
+                return "comment";
+            }
+            
+            if (state.atSep2) {
+                state.componentSeparator = stream.next();
+                state.atSep2 = false;
+                state.atSep3 = true;
+                return "comment";
+            }
+
+            if (state.atSep3) {
+                state.fieldRepeater = stream.next();
+                state.atSep3 = false;
+                state.atSep4 = true;
+                return "comment";
+            }
+
+            if (state.atSep4) {
+                state.escapeCharacter = stream.next();
+                state.atSep4 = false;
+                state.atSep5 = true;
+                return "comment";
+            }
+
+            if (state.atSep5) {
+                state.subComponentSeparator = stream.next();
+                state.atSep5 = false;
+                return "comment";
+            }
+
+            if (stream.sol()) {
                 state.currentField = 0;
                 state.firstSepEncountered = false;
                 state.segmentName = '';
             }
+            
 
             if (!state.firstSepEncountered) {
                 var curChar = stream.eat(function(c) { return c != state.fieldSeparator; });
+
                 if (curChar != null) {
                     state.segmentName += curChar;
-                    return "string";    
+                    
+                    if (state.segmentName == "MSH")
+                        state.atSep1 = true;
+
+                    return "string";
                 }
             }
 
             state.firstSepEncountered = true;
-            if (stream.next() == state.fieldSeparator) {
+
+            var nextChar = stream.peek();
+
+            if (nextChar == state.fieldSeparator) {
                 state.currentField += 1;
+                stream.next();
+                return "comment";
+            } else if (nextChar == state.componentSeparator) {
+                stream.next();
+                return "comment";
+            } else if (nextChar == state.fieldRepeater) {
+                stream.next();
+                return "comment";
+            } else if (nextChar == state.escapeCharacter) {
+                stream.next();
+                return "comment";
+            } else if (nextChar == state.subComponentSeparator) {
+                stream.next();
                 return "comment";
             } else {
-                stream.eatWhile(function(c) { return c != state.fieldSeparator; });
+                stream.eatWhile(function (c) { 
+                    return c != state.fieldSeparator  && 
+                        c != state.componentSeparator && 
+                        c != state.fieldRepeater && 
+                        c != state.escapeCharacter && 
+                        c != state.subComponentSeparator; 
+                });
 
                 if (state.segmentName == "MSH" && state.currentField == 8)
                     return "variable";
--- js/main.js
+++ js/main.js
@@ -1,7 +1,61 @@
 var fs = require('fs');
 var gui = require('nw.gui');
 
-document.addEventListener('DOMContentLoaded', function() {
+
+var clickInput = function (id) {
+    var event = document.createEvent('MouseEvents');
+    event.initMouseEvent('click');
+    document.getElementById(id).dispatchEvent(event);
+}
+
+var viewModel = (function () {
+    var obj = {};
+
+    obj.currentSegment = ko.observable({ name: ko.observable(), fields: ko.observableArray() });
+
+    obj.currentLineTextUpdated = function (mshLineText, lineText) {
+        if(mshLineText != null) {
+            try {
+                var fieldSep = mshLineText.substring(3, 4);
+                var compSep = mshLineText.substring(4, 5);
+                var repeatChar = mshLineText.substring(5, 6);
+                var escapeChar = mshLineText.substring(6, 7);
+                var subCompSep = mshLineText.substring(7, 8);
+                
+                var fields = lineText.split(fieldSep);
+                obj.currentSegment().name(fields[0]);
+                obj.currentSegment().fields.removeAll();
+                                
+                for (var i in fields) {
+                    var cmps = [];
+                    if (fields[i].indexOf(compSep) >= 0) {
+                        var components = fields[i].split(compSep);
+
+                        for (var j in components) {
+                            var comp = components[j];
+                            console.log(comp)
+                            cmps.push(comp);
+                        }
+                    }
+
+                    var fieldDef = { idx: i, text: fields[i], components: cmps };
+                    
+                    
+
+                    obj.currentSegment().fields.push(fieldDef);
+                } 
+
+            } catch (ex) { 
+                console.log("Error loading line: " + ex);
+            }
+        }
+    };
+
+    return obj;
+})();
+
+
+document.addEventListener('DOMContentLoaded', function () {
     var ed = CodeMirror(document.getElementById('editorContainer'), 
                         {
                             mode: 'hl7',
@@ -10,15 +64,82 @@ document.addEventListener('DOMContentLoaded', function() {
                             lineNumbers: true
                         });
     ed.setSize(window.innerWidth * 0.66, window.innerHeight);
+    ed.on("cursorActivity", function (doc) {
+        var lineNumber = ed.getCursor().line;
+        var lineText = doc.getRange({ line: lineNumber, ch: 0 }, { line: lineNumber + 1, ch: 0 });
+        var mshLineText = null;
+
+        for (var i = lineNumber; i >= 0; i--) {
+            var curLineText = doc.getRange({ line: i, ch: 0 }, { line: i + 1, ch: 0 });
+
+            if (curLineText.indexOf('MSH') == 0) {
+                mshLineText = curLineText;
+                break;
+            }
+        }
+
+        viewModel.currentLineTextUpdated(mshLineText, lineText);
+    });
 
     window.addEventListener('resize', function () {
         ed.setSize(window.innerWidth * 0.66, window.innerHeight);
     });
+
+
+    document.addEventListener('keyup', function (e) {
+        if (e.keyCode == 'O'.charCodeAt(0) && e.ctrlKey) {
+            clickInput('open');
+        } else if (e.keyCode == 'S'.charCodeAt(0) && e.ctrlKey) {
+            clickInput('save');
+        }
+    });
+
+    document.getElementById('open').addEventListener('change', function (e) {
+        fs.readFile(this.value, 'utf-8', function (error, contents) {
+            try {
+                ed.setValue(contents); 
+            } catch (ex) {}
+        });
+    });
+     
+    document.getElementById('save').addEventListener('change', function (e) {
+        try {
+            var text = ed.getValue();
+            fs.writeFile(this.value, text);
+        } catch (ex) {}
+    });
+
+    var menu = new gui.Menu({ type: 'menubar' });
+    menu.append(new gui.MenuItem({
+        label: 'File',
+        submenu: new gui.Menu()
+    }));
+
+    menu.items[0].submenu.append(new gui.MenuItem({
+        label: 'Open',
+        click: function () {
+            clickInput('open');
+        }
+    }));
+    menu.items[0].submenu.append(new gui.MenuItem({
+        label: 'Save',
+        click: function () {
+            clickInput('save');
+        }
+    }));
+    menu.items[0].submenu.append(new gui.MenuItem({
+        type: 'separator'
+    }));
+    menu.items[0].submenu.append(new gui.MenuItem({
+        label: 'Close',
+        click: function () {
+            gui.Window.get().close();
+        }
+    }));
+    gui.Window.get().menu = menu;
+
+    ko.applyBindings(viewModel);
 });
 
 
-
-
-
-
-
+ 
